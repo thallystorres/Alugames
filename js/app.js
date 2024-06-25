42811b96c291f8ff5f5e76baac315351a27ad64f
Thallys Torres <thallys.auugusto.torres@gmail.com>
2024-06-25 02:48:51 -0300

    Jogo atualizado

--- js/app.js
+++ js/app.js
@@ -1,14 +1,22 @@
+let contador = 1;
+console.log(`${contador} jogo foi alugado`);
 function alterarStatus(id) {
     let jogoClicado = document.getElementById(`game-${id}`);
     let imagem = jogoClicado.querySelector('.dashboard__item__img');
     let botao = jogoClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = jogoClicado.querySelector('.dashboard__item__name');
     if (imagem.classList.contains('dashboard__item__img--rented')) {
-        botao.classList.remove('dashboard__item__button--return');
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.textContent = 'Alugar'
-    } else {
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
+            botao.classList.remove('dashboard__item__button--return');
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.textContent = 'Alugar'
+            contador --
+            console.log(contador == 1 ? `${contador} jogo alugado` : `${contador} jogos alugados`);
+    }} else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
-        botao.textContent = 'Devolver'
+        botao.textContent = 'Devolver';
+        contador++;
+        console.log(contador == 1 ?`${contador} jogo alugado` : `${contador} jogos alugados`);
     }
 }
