faff5664501fdf894fab90ead4a466e05cf15469
Thallys Torres <thallys.auugusto.torres@gmail.com>
2024-06-25 01:57:33 -0300

    primeiro commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+function alterarStatus(id) {
+    let jogoClicado = document.getElementById(`game-${id}`);
+    let imagem = jogoClicado.querySelector('.dashboard__item__img');
+    let botao = jogoClicado.querySelector('.dashboard__item__button');
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        botao.classList.remove('dashboard__item__button--return');
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.textContent = 'Alugar'
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver'
+    }
+}
